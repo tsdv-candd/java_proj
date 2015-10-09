a631bc1e8a639feb46aefaffee2e10c90fb76720
baonk <khacbao1991@gmail.com>
2015-10-09 23:43:50 +0900

    Add changed

--- NRL/src/Fixture.java
+++ NRL/src/Fixture.java
@@ -1,4 +1,72 @@
+import java.util.Date;
 
 public class Fixture {
-
+     private int matchNumber;
+     private int roundNumber;
+     private String homeTeamName;
+     private String awayTeamName;
+     private Date matchDate;
+     private String matchVenue;
+     private int matchTime;
+     private int homeTeamScore;
+     private int awayTeamScore;
+     
+     
+	public int getMatchNumber() {
+		return matchNumber;
+	}
+	public void setMatchNumber(int matchNumber) {
+		this.matchNumber = matchNumber;
+	}
+	public int getRoundNumber() {
+		return roundNumber;
+	}
+	public void setRoundNumber(int roundNumber) {
+		this.roundNumber = roundNumber;
+	}
+	public String getHomeTeamName() {
+		return homeTeamName;
+	}
+	public void setHomeTeamName(String homeTeamName) {
+		this.homeTeamName = homeTeamName;
+	}
+	public String getAwayTeamName() {
+		return awayTeamName;
+	}
+	public void setAwayTeamName(String awayTeamName) {
+		this.awayTeamName = awayTeamName;
+	}
+	public Date getMatchDate() {
+		return matchDate;
+	}
+	public void setMatchDate(Date matchDate) {
+		this.matchDate = matchDate;
+	}
+	public String getMatchVenue() {
+		return matchVenue;
+	}
+	public void setMatchVenue(String matchVenue) {
+		this.matchVenue = matchVenue;
+	}
+	public int getMatchTime() {
+		return matchTime;
+	}
+	public void setMatchTime(int matchTime) {
+		this.matchTime = matchTime;
+	}
+	public int getHomeTeamScore() {
+		return homeTeamScore;
+	}
+	public void setHomeTeamScore(int homeTeamScore) {
+		this.homeTeamScore = homeTeamScore;
+	}
+	public int getAwayTeamScore() {
+		return awayTeamScore;
+	}
+	public void setAwayTeamScore(int awayTeamScore) {
+		this.awayTeamScore = awayTeamScore;
+	}
+     
+     
+     
 }
--- NRL/src/Team.java
+++ NRL/src/Team.java
@@ -1,4 +1,83 @@
 
 public class Team {
-
+	
+	private String teamName;
+	private String teamMascot;
+	private String homeGround;
+	private int rank;
+	private int numberGamePlayeds;
+	private int numberGameWons;
+	private int numberGameLosts;
+	private int numberOfByes;
+	private int pointsScoreFor;
+	private int pointsScoreAgainst;
+	private int totalPoints;
+	
+	
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	public String getTeamMascot() {
+		return teamMascot;
+	}
+	public void setTeamMascot(String teamMascot) {
+		this.teamMascot = teamMascot;
+	}
+	public String getHomeGround() {
+		return homeGround;
+	}
+	public void setHomeGround(String homeGround) {
+		this.homeGround = homeGround;
+	}
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	public int getNumberGamePlayeds() {
+		return numberGamePlayeds;
+	}
+	public void setNumberGamePlayeds(int numberGamePlayeds) {
+		this.numberGamePlayeds = numberGamePlayeds;
+	}
+	public int getNumberGameWons() {
+		return numberGameWons;
+	}
+	public void setNumberGameWons(int numberGameWons) {
+		this.numberGameWons = numberGameWons;
+	}
+	public int getNumberGameLosts() {
+		return numberGameLosts;
+	}
+	public void setNumberGameLosts(int numberGameLosts) {
+		this.numberGameLosts = numberGameLosts;
+	}
+	public int getNumberOfByes() {
+		return numberOfByes;
+	}
+	public void setNumberOfByes(int numberOfByes) {
+		this.numberOfByes = numberOfByes;
+	}
+	public int getPointsScoreFor() {
+		return pointsScoreFor;
+	}
+	public void setPointsScoreFor(int pointsScoreFor) {
+		this.pointsScoreFor = pointsScoreFor;
+	}
+	public int getPointsScoreAgainst() {
+		return pointsScoreAgainst;
+	}
+	public void setPointsScoreAgainst(int pointsScoreAgainst) {
+		this.pointsScoreAgainst = pointsScoreAgainst;
+	}
+	public int getTotalPoints() {
+		return totalPoints;
+	}
+	public void setTotalPoints(int totalPoints) {
+		this.totalPoints = totalPoints;
+	}
 }
