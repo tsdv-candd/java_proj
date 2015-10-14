9c2f13132422a0e9f1b9d97382fda92e7fc759b3
CanDD <canddk44bk@gmail.com>
2015-10-15 00:00:24 +0900

    Add comment for importance methods

--- NRL/src/Fixture_18219249.java
+++ NRL/src/Fixture_18219249.java
@@ -1,133 +1,141 @@
-/*
- * Student ID: 18219249 Name: Mohsen Mirhashemi Campus: PT parramatta Campus Tutor Name: Indra Class
- * Day: Thursdays Class Time: 12:00-14:00
- */
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/*
+ * Student ID:  18219249
+ * Name:        Mohsen Mirhashemi
+ * Campus:      PT parramatta Campus
+ * Tutor Name:  Indra 
+ * Class Day:   Thursdays
+ * Class Time:  12:00-14:00
+ */
 public class Fixture_18219249 {
-  private int matchNumber;
-  private int roundNumber;
-  private String homeTeamName;
-  private String awayTeamName;
-  private Date matchDate;
-  private String matchVenue;
-  private Date kickoffTime;
-  private int homeTeamScore;
-  private int awayTeamScore;
+	private int matchNumber;
+	private int roundNumber;
+	private String homeTeamName;
+	private String awayTeamName;
+	private Date matchDate;
+	private String matchVenue;
+	private Date kickoffTime;
+	private int homeTeamScore;
+	private int awayTeamScore;
 
-  public Fixture_18219249(int matchNumber, int roundNumber, String homeName, String awayName,
-      String Venue, Date kickoffTime, Date date) {
-    this.matchNumber = matchNumber;
-    this.roundNumber = roundNumber;
-    this.homeTeamName = homeName;
-    this.awayTeamName = awayName;
-    this.matchVenue = Venue;
-    this.kickoffTime = kickoffTime;
-    this.matchDate = date;
-  }
+	public Fixture_18219249(int matchNumber, int roundNumber, String homeName,
+			String awayName, String Venue, Date kickoffTime, Date date) {
+		this.matchNumber = matchNumber;
+		this.roundNumber = roundNumber;
+		this.homeTeamName = homeName;
+		this.awayTeamName = awayName;
+		this.matchVenue = Venue;
+		this.kickoffTime = kickoffTime;
+		this.matchDate = date;
+	}
 
+	public int getMatchNumber() {
+		return matchNumber;
+	}
 
-  public int getMatchNumber() {
-    return matchNumber;
-  }
+	public void setMatchNumber(int matchNumber) {
+		this.matchNumber = matchNumber;
+	}
 
-  public void setMatchNumber(int matchNumber) {
-    this.matchNumber = matchNumber;
-  }
+	public int getRoundNumber() {
+		return roundNumber;
+	}
 
-  public int getRoundNumber() {
-    return roundNumber;
-  }
+	public void setRoundNumber(int roundNumber) {
+		this.roundNumber = roundNumber;
+	}
 
-  public void setRoundNumber(int roundNumber) {
-    this.roundNumber = roundNumber;
-  }
+	public String getHomeTeamName() {
+		return homeTeamName;
+	}
 
-  public String getHomeTeamName() {
-    return homeTeamName;
-  }
+	public void setHomeTeamName(String homeTeamName) {
+		this.homeTeamName = homeTeamName;
+	}
 
-  public void setHomeTeamName(String homeTeamName) {
-    this.homeTeamName = homeTeamName;
-  }
+	public String getAwayTeamName() {
+		return awayTeamName;
+	}
 
-  public String getAwayTeamName() {
-    return awayTeamName;
-  }
+	public void setAwayTeamName(String awayTeamName) {
+		this.awayTeamName = awayTeamName;
+	}
 
-  public void setAwayTeamName(String awayTeamName) {
-    this.awayTeamName = awayTeamName;
-  }
+	public Date getMatchDate() {
+		return matchDate;
+	}
 
-  public Date getMatchDate() {
-    return matchDate;
-  }
+	public void setMatchDate(Date matchDate) {
+		this.matchDate = matchDate;
+	}
 
-  public void setMatchDate(Date matchDate) {
-    this.matchDate = matchDate;
-  }
+	public String getMatchVenue() {
+		return matchVenue;
+	}
 
-  public String getMatchVenue() {
-    return matchVenue;
-  }
+	public void setMatchVenue(String matchVenue) {
+		this.matchVenue = matchVenue;
+	}
 
-  public void setMatchVenue(String matchVenue) {
-    this.matchVenue = matchVenue;
-  }
+	public Date getKickoffTime() {
+		return kickoffTime;
+	}
 
-  public Date getKickoffTime() {
-    return kickoffTime;
-  }
+	public void setKickoffTime(Date kickoffTime) {
+		this.kickoffTime = kickoffTime;
+	}
 
-  public void setKickoffTime(Date kickoffTime) {
-    this.kickoffTime = kickoffTime;
-  }
+	public int getHomeTeamScore() {
+		return homeTeamScore;
+	}
 
-  public int getHomeTeamScore() {
-    return homeTeamScore;
-  }
+	public void setHomeTeamScore(int homeTeamScore) {
+		this.homeTeamScore = homeTeamScore;
+	}
 
-  public void setHomeTeamScore(int homeTeamScore) {
-    this.homeTeamScore = homeTeamScore;
-  }
+	public int getAwayTeamScore() {
+		return awayTeamScore;
+	}
 
-  public int getAwayTeamScore() {
-    return awayTeamScore;
-  }
+	public void setAwayTeamScore(int awayTeamScore) {
+		this.awayTeamScore = awayTeamScore;
+	}
 
-  public void setAwayTeamScore(int awayTeamScore) {
-    this.awayTeamScore = awayTeamScore;
-  }
-
-  public void display(boolean isHomeTeam) {
-    String leftAlignFormat = " %-2d\t%-20s\t%-20s\t%-10s\t%02d-%02d%n";   
-    final DateFormat df2 = new SimpleDateFormat("dd/MM/yy");
-    String date = df2.format(this.getMatchDate());
-    if (isHomeTeam) {
-      String status;
-      if (homeTeamScore > awayTeamScore) {
-        status = "Win";
-      } else if (homeTeamScore < awayTeamScore) {
-        status = "Loss";
-      } else {
-        status = "Draw";
-      }
-      System.out.format(leftAlignFormat, roundNumber, date, awayTeamName, status, homeTeamScore,
-          awayTeamScore);
-    } else {
-      String status;
-      if (homeTeamScore > awayTeamScore) {
-        status = "Loss";
-      } else if (homeTeamScore < awayTeamScore) {
-        status = "Win";
-      } else {
-        status = "Draw";
-      }
-      System.out.format(leftAlignFormat, roundNumber, date, homeTeamName, status, homeTeamScore,
-          awayTeamScore);
-    }
-  }
+	/*******************************************************************************
+	 * @method name: display
+	 * @brief : Display team result. (Main process for the menu No.4)
+	 * @param : boolean isHomeTeam for checking the match at the home team or not.
+	 * @retval : None
+	 *****************************************************************************/
+	public void display(boolean isHomeTeam) {
+		String leftAlignFormat = " %-2d\t%-20s\t%-20s\t%-10s\t%02d-%02d%n";
+		final DateFormat df2 = new SimpleDateFormat("dd/MM/yy");
+		String date = df2.format(this.getMatchDate());
+		if (isHomeTeam) {
+			String status;
+			if (homeTeamScore > awayTeamScore) {
+				status = "Win";
+			} else if (homeTeamScore < awayTeamScore) {
+				status = "Loss";
+			} else {
+				status = "Draw";
+			}
+			System.out.format(leftAlignFormat, roundNumber, date, awayTeamName,
+					status, homeTeamScore, awayTeamScore);
+		} else {
+			String status;
+			if (homeTeamScore > awayTeamScore) {
+				status = "Loss";
+			} else if (homeTeamScore < awayTeamScore) {
+				status = "Win";
+			} else {
+				status = "Draw";
+			}
+			System.out.format(leftAlignFormat, roundNumber, date, homeTeamName,
+					status, homeTeamScore, awayTeamScore);
+		}
+	}
 }
--- NRL/src/Team_18219249.java
+++ NRL/src/Team_18219249.java
@@ -6,194 +6,217 @@
  * Class Day:   Thursdays
  * Class Time:  12:00-14:00
  */
-
 public class Team_18219249 implements Comparable<Team_18219249> {
 
-  private String teamName;
-  private String teamMascot;
-  private String homeGround;
-  private int rank;
-  private int numberGamePlayeds;
-  private int numberGameWons;
-  private int numberGameLosts;
-  private int numberOfByes;
-  private int pointsScoreFor;
-  private int pointsScoreAgainst;
-  private int totalPoints;
+	private String teamName;
+	private String teamMascot;
+	private String homeGround;
+	private int rank;
+	private int numberGamePlayeds;
+	private int numberGameWons;
+	private int numberGameLosts;
+	private int numberOfByes;
+	private int pointsScoreFor;
+	private int pointsScoreAgainst;
+	private int totalPoints;
 
-  public Team_18219249(String teamName, String teamMascot, String homeGround) {
-    this.teamName = teamName;
-    this.teamMascot = teamMascot;
-    this.homeGround = homeGround;  
-  }
+	public Team_18219249(String teamName, String teamMascot, String homeGround) {
+		this.teamName = teamName;
+		this.teamMascot = teamMascot;
+		this.homeGround = homeGround;
+	}
 
-  public String getTeamName() {
-    return teamName;
-  }
+	public String getTeamName() {
+		return teamName;
+	}
 
-  public void setTeamName(String teamName) {
-    this.teamName = teamName;
-  }
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
 
-  public String getTeamMascot() {
-    return teamMascot;
-  }
+	public String getTeamMascot() {
+		return teamMascot;
+	}
 
-  public void setTeamMascot(String teamMascot) {
-    this.teamMascot = teamMascot;
-  }
+	public void setTeamMascot(String teamMascot) {
+		this.teamMascot = teamMascot;
+	}
 
-  public String getHomeGround() {
-    return homeGround;
-  }
+	public String getHomeGround() {
+		return homeGround;
+	}
 
-  public void setHomeGround(String homeGround) {
-    this.homeGround = homeGround;
-  }
+	public void setHomeGround(String homeGround) {
+		this.homeGround = homeGround;
+	}
 
-  public int getRank() {
-    return rank;
-  }
+	public int getRank() {
+		return rank;
+	}
 
-  public void setRank(int rank) {
-    this.rank = rank;
-  }
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
 
-  public int getNumberGamePlayeds() {
-    return numberGamePlayeds;
-  }
+	public int getNumberGamePlayeds() {
+		return numberGamePlayeds;
+	}
 
-  public void setNumberGamePlayeds(int numberGamePlayeds) {
-    this.numberGamePlayeds = numberGamePlayeds;
-  }
+	public void setNumberGamePlayeds(int numberGamePlayeds) {
+		this.numberGamePlayeds = numberGamePlayeds;
+	}
 
-  public int getNumberGameWons() {
-    return numberGameWons;
-  }
+	public int getNumberGameWons() {
+		return numberGameWons;
+	}
 
-  public void setNumberGameWons(int numberGameWons) {
-    this.numberGameWons = numberGameWons;
-  }
+	public void setNumberGameWons(int numberGameWons) {
+		this.numberGameWons = numberGameWons;
+	}
 
-  public int getNumberGameLosts() {
-    return numberGameLosts;
-  }
+	public int getNumberGameLosts() {
+		return numberGameLosts;
+	}
 
-  public void setNumberGameLosts(int numberGameLosts) {
-    this.numberGameLosts = numberGameLosts;
-  }
+	public void setNumberGameLosts(int numberGameLosts) {
+		this.numberGameLosts = numberGameLosts;
+	}
 
-  public int getNumberOfByes() {
-    return numberOfByes;
-  }
+	public int getNumberOfByes() {
+		return numberOfByes;
+	}
 
-  public void setNumberOfByes(int numberOfByes) {
-    this.numberOfByes = numberOfByes;
-  }
+	public void setNumberOfByes(int numberOfByes) {
+		this.numberOfByes = numberOfByes;
+	}
 
-  public int getPointsScoreFor() {
-    return pointsScoreFor;
-  }
+	public int getPointsScoreFor() {
+		return pointsScoreFor;
+	}
 
-  public void setPointsScoreFor(int pointsScoreFor) {
-    this.pointsScoreFor = pointsScoreFor;
-  }
+	public void setPointsScoreFor(int pointsScoreFor) {
+		this.pointsScoreFor = pointsScoreFor;
+	}
 
-  public int getPointsScoreAgainst() {
-    return pointsScoreAgainst;
-  }
+	public int getPointsScoreAgainst() {
+		return pointsScoreAgainst;
+	}
 
-  public void setPointsScoreAgainst(int pointsScoreAgainst) {
-    this.pointsScoreAgainst = pointsScoreAgainst;
-  }
+	public void setPointsScoreAgainst(int pointsScoreAgainst) {
+		this.pointsScoreAgainst = pointsScoreAgainst;
+	}
 
-  public int getTotalPoints() {
-    return totalPoints;
-  }
+	public int getTotalPoints() {
+		return totalPoints;
+	}
 
-  public void setTotalPoints(int totalPoints) {
-    this.totalPoints = totalPoints;
-  }
+	public void setTotalPoints(int totalPoints) {
+		this.totalPoints = totalPoints;
+	}
 
-  public void update(int homeScore, int awayScore, int check) {
-    if (check == 3) {
-      this.numberOfByes += 1;
-    } else {
-      numberGamePlayeds += 1;
-      if (check == 1) {
-        this.pointsScoreFor += homeScore;
-        this.pointsScoreAgainst += awayScore;
-        if (homeScore > awayScore) {
-          numberGameWons += 1;
-        } else if (homeScore < awayScore) {
-          numberGameLosts += 1;
-        }
-      }
-      if (check == 2) {
-        this.pointsScoreFor += awayScore;
-        this.pointsScoreAgainst += homeScore;
-        if (awayScore > homeScore) {
-          numberGameWons += 1;
-        } else if (awayScore < homeScore) {
-          numberGameLosts += 1;
-        }
-      }
-    }
+	/*******************************************************************************
+	 * @method name: update
+	 * @brief : Update team score
+	 * @param : int homeScore team score earn at their home's Stadium.
+	 * @param : int awayScore team score earn at away Stadium.
+	 * @param : int check check if the match is byes, at home or at away
+	 *          stadium.
+	 * @retval : None
+	 *****************************************************************************/
+	public void update(int homeScore, int awayScore, int check) {
+		if (check == 3) {
+			this.numberOfByes += 1;
+		} else {
+			numberGamePlayeds += 1;
+			if (check == 1) {
+				this.pointsScoreFor += homeScore;
+				this.pointsScoreAgainst += awayScore;
+				if (homeScore > awayScore) {
+					numberGameWons += 1;
+				} else if (homeScore < awayScore) {
+					numberGameLosts += 1;
+				}
+			}
+			if (check == 2) {
+				this.pointsScoreFor += awayScore;
+				this.pointsScoreAgainst += homeScore;
+				if (awayScore > homeScore) {
+					numberGameWons += 1;
+				} else if (awayScore < homeScore) {
+					numberGameLosts += 1;
+				}
+			}
+		}
+		int numberGameDraws = numberGamePlayeds
+				- (numberGameWons + numberGameLosts);
+		totalPoints = 2 * (numberGameWons + numberOfByes) + 1 * numberGameDraws;
+	}
 
+	/*******************************************************************************
+	 * @method name: compareTo
+	 * @brief : Compare the result with other team to update rank (ladder)
+	 * @param : Team_18219249 otherTeam input team to be compare with
+	 * @retval : return 0 if two team's total score are equal, return > 0 if
+	 *         this team's total score is higher return < 0 if this team's total
+	 *         score is lower than input team.
+	 *****************************************************************************/
+	public int compareTo(Team_18219249 otherTeam) {
+		if (totalPoints == otherTeam.totalPoints) {
+			int temp1 = pointsScoreFor - pointsScoreAgainst;
+			int temp2 = otherTeam.pointsScoreFor - otherTeam.pointsScoreAgainst;
+			if (temp1 == temp2) {
+				if (pointsScoreFor == otherTeam.pointsScoreFor) {
+					return teamName.compareToIgnoreCase(otherTeam.teamName);
+				} else {
+					return (pointsScoreFor - otherTeam.pointsScoreFor);
+				}
+			} else {
+				return (temp1 - temp2);
+			}
+		} else {
+			return (totalPoints - otherTeam.totalPoints);
+		}
+	}
 
-    int numberGameDraws = numberGamePlayeds - (numberGameWons + numberGameLosts);
-    totalPoints = 2 * (numberGameWons + numberOfByes) + 1 * numberGameDraws;
-    
-  }
+	/*******************************************************************************
+	 * @method name: updateRank
+	 * @brief : Update ranking for all team
+	 * @param : Team_18219249[] t reference to the team list
+	 * @retval None.
+	 *****************************************************************************/
+	public void updateRank(Team_18219249[] t) {
+		int i = 0;
+		while (!teamName.equals(t[i].getTeamName())) {
+			i++;
+		}
+		this.setRank(i + 1);
+	}
 
-  @Override
-  public int compareTo(Team_18219249 otherTeam) {
-    if (totalPoints == otherTeam.totalPoints) {
-      int temp1 = pointsScoreFor - pointsScoreAgainst;
-      int temp2 = otherTeam.pointsScoreFor - otherTeam.pointsScoreAgainst;
-      if (temp1 == temp2) {
-        if (pointsScoreFor == otherTeam.pointsScoreFor) {
-          return teamName.compareToIgnoreCase(otherTeam.teamName);
-        } else {
-          return (pointsScoreFor - otherTeam.pointsScoreFor);
-        }
-      } else {
-        return (temp1 - temp2);
-      }
-    } else {
-      return (totalPoints - otherTeam.totalPoints);
-    }
-
-  }
-
-  public void updateRank(Team_18219249[] t) {
-    int i = 0;
-    while (!teamName.equals(t[i].getTeamName())) {
-      i++;
-    }
-    this.setRank(i + 1);
-  }
-
-  public void showResult(int currentRound, Fixture_18219249[][] fx) {
-    for (int i = 0; i < currentRound; i++) {
-      for (int j = 0; j < 8; j++) {
-        if (fx[i][j] != null) {
-          if (teamName.equals(fx[i][j].getHomeTeamName())) {
-            fx[i][j].display(true);
-          }
-          if (teamName.equals(fx[i][j].getAwayTeamName())) {
-            fx[i][j].display(false);
-          }
-        } else {
-          continue;
-        }
-      }
-    }
-    System.out.println();
-    System.out.println("Total competition poins after " + currentRound + " rounds: " + totalPoints);
-    System.out.println();
-  }
-
-
-
+	/*******************************************************************************
+	 * @method name: showResult
+	 * @brief : Show team result until the current round.
+	 * @param : int currentRound Current round number which user input at
+	 *        startup
+	 * @retval None.
+	 *****************************************************************************/
+	public void showResult(int currentRound, Fixture_18219249[][] fx) {
+		for (int i = 0; i < currentRound; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (fx[i][j] != null) {
+					if (teamName.equals(fx[i][j].getHomeTeamName())) {
+						fx[i][j].display(true);
+					}
+					if (teamName.equals(fx[i][j].getAwayTeamName())) {
+						fx[i][j].display(false);
+					}
+				} else {
+					continue;
+				}
+			}
+		}
+		System.out.println();
+		System.out.println("Total competition poins after " + currentRound
+				+ " rounds: " + totalPoints);
+		System.out.println();
+	}
 }
